61677e35b4a5eaff9464b57e16c54c9fed64126c
valerij-karimov <134427942+valerij-karimov@users.noreply.github.com>
2023-06-11 08:21:32 +0700

    добавил вторую домашку
    проиндексировал список песен

---
+++ task_1.1.py
@@ -0,0 +1,5 @@
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+print(f'первый трек - {my_favorite_songs[0:13]}')
+print(f'последний трек - {my_favorite_songs[-13:-1]}')
+print(f'второй трек - {my_favorite_songs[15 : 30]}')
+print(f'второй с конца трек - {my_favorite_songs[51 : -15]}')
